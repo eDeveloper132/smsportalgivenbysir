e7a4453dbbd548dba3a26e206de6888ec29b684e
eDeveloper132 <1375219.aptechiic@gmail.com>
2024-09-23 14:28:30 +0500

    first

---
+++ Routes/Main.ts
@@ -0,0 +1,114 @@
+import express, { Request, Response } from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import bcrypt from "bcrypt";
+import axios from "axios";
+import { AppRes } from "../index.js";
+
+// Resolve file and directory paths
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const router = express.Router();
+const findAndUpdateUserById = async (id: string, updateData: any) => {
+  try {
+    // Find the user by id
+    const responseFind = await axios.post(
+      "https://ap-southeast-1.aws.data.mongodb-api.com/app/data-mdipydh/endpoint/data/v1/action/findOne",
+      {
+        collection: "signhandlers", // Replace with your actual collection name
+        database: "test", // Replace with your actual database name
+        dataSource: "SMSCluster", // Replace with your actual data source name
+        filter: { _id: { $oid: id } }, // Filter to find the user by id
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "api-key": process.env.MongoDB_API_KEY, // Ensure this is set in your environment variables
+        },
+      }
+    );
+
+    // Check if the user exists
+    const user = responseFind.data.document;
+    if (!user) {
+      return { error: "User not found." };
+    }
+
+    // Update the user with the new data
+    const responseUpdate = await axios.post(
+      "https://ap-southeast-1.aws.data.mongodb-api.com/app/data-mdipydh/endpoint/data/v1/action/updateOne",
+      {
+        collection: "signhandlers", // Replace with your actual collection name
+        database: "test", // Replace with your actual database name
+        dataSource: "SMSCluster", // Replace with your actual data source name
+        filter: { _id: { $oid: id } }, // Filter to find the user by id
+        update: {
+          $set: updateData, // Update with the new data
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "api-key": process.env.MongoDB_API_KEY, // Ensure this is set in your environment variables
+        },
+      }
+    );
+
+    return responseUpdate.data; // Return the result of the update operation
+  } catch (error: any) {
+    console.error(
+      "Error finding and updating user by id:",
+      error.response ? error.response.data : error.message
+    );
+    throw new Error("Failed to find and update user by id.");
+  }
+};
+// Route to serve the HTML page
+router.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "../Views/index.html"));
+});
+
+router.get("/changepass", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../Views/changePass.html"));
+});
+
+router.post("/changepass", async (req: Request, res: AppRes) => {
+  const { current_password, new_password, confirm_password } = req.body;
+
+  if (!current_password || !new_password || !confirm_password) {
+    return res.status(400).send("All fields are required.");
+  }
+
+  // Fetch user details from FetchUserDetails
+  const user = res.locals.user; // Modify as needed
+  if (!user) {
+    return res.status(404).send("User not found.");
+  }
+
+  try {
+    // Verify current password
+    const match = await bcrypt.compare(current_password, user.Password!);
+    if (!match) {
+      return res.status(400).send("Current password is incorrect.");
+    }
+
+    // Hash new password
+    const hashedPassword = await bcrypt.hash(new_password, 10);
+    const updated_data = {
+      Password: hashedPassword,
+    };
+
+    // Update user password
+    await findAndUpdateUserById(user._id as string, updated_data);
+
+    res.send("Password changed successfully.");
+  } catch (error: any) {
+    console.error("Error changing password:", error);
+    res
+      .status(500)
+      .send({ error: "Error changing password: " + error.message });
+  }
+});
+
+export default router;
---
+++ Routes/SMS.ts
@@ -0,0 +1,159 @@
+import express, { Request, Response } from "express";
+import "dotenv/config";
+import { fileURLToPath } from "url";
+import path from "path";
+import axios from "axios";
+import { MessageModel } from "../Schema/Post.js";
+import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+import { SignModel } from "../Schema/Post.js";
+import { AppRes } from "../index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const router = express.Router();
+
+// Route to serve the HTML file
+router.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "../Views/sms.html"));
+});
+
+// POST route to handle SMS sending
+router.post("/", async (req: Request, res: AppRes) => {
+  const { phonecode, phonenumber, message } = req.body;
+
+  if (!phonecode || !phonenumber || !message) {
+    console.log("Server Error 400: Missing required fields");
+    return res.status(400).json({
+      error:
+        "Please fill in all the required fields: phone code, phone number, and message.",
+    });
+  }
+
+  const user = res.locals.user;
+  if (!user) {
+    return res.status(404).send("User not found.");
+  }
+  const packageName = user?.Details?.PackageName;
+  const coins = user?.Details?.Coins;
+
+  if (!packageName || !coins) {
+    console.log("Server Error 403: User package details are incomplete.");
+    return res
+      .status(403)
+      .json({ error: "You cannot send SMS. Please buy our package first." });
+  }
+
+  const mix = `${phonecode}${phonenumber}`;
+  console.log(`We are delivering this message: ${message} to ${mix}`);
+
+  try {
+    // Send SMS using ClickSend API
+    const smsMessage = {
+      to: mix,
+      body: message,
+    };
+
+    const apiUrl = "https://rest.clicksend.com/v3/sms/send";
+    const response = await axios.post(
+      apiUrl,
+      {
+        messages: [smsMessage],
+      },
+      {
+        auth: {
+          username: "bluebirdintegrated@gmail.com",
+          password: "EA26A5D0-7AAC-6631-478B-FC155CE94C99",
+        },
+      }
+    );
+
+    console.log(response.data);
+
+    const userData = user;
+    const userId = userData._id;
+    const dbUser = await SignModel.findById(userId);
+
+    if (!dbUser) {
+      return res.status(404).send("User not found");
+    }
+
+    if (!dbUser.Details) {
+      return res.status(400).send("User details not found");
+    }
+
+    // Deduct one coin from the user's balance
+    let coins = dbUser.Details.Coins;
+    if (typeof coins === "number") {
+      coins -= 1;
+      if (coins <= 0) {
+        return res.status(400).send("Insufficient coins for sending message");
+      }
+      dbUser.Details.Coins = coins;
+    }
+
+    // Create a new message entry in the database
+    const newMessage = await MessageModel.create({
+      id: uuidv4(),
+      u_id: dbUser._id,
+      from: "NOT PROVIDED",
+      to: mix,
+      message: message,
+      m_count: response.data.data.total_count,
+      m_schedule: "NOT PROVIDED",
+      status: response.data.response_code,
+    });
+
+    // Cast newMessage._id to ObjectId
+    const messageId = newMessage._id as mongoose.Types.ObjectId;
+
+    // Add the message to the user's messages array and save the user
+    dbUser.messages.push(messageId);
+    await dbUser.save();
+
+    console.log("Data Updated Successfully", dbUser);
+    console.log("Data Added Successfully", newMessage);
+
+    // Respond with success
+    res.status(200).json({ message: "Message sent successfully!" });
+  } catch (err: any) {
+    console.error(err.response ? err.response.data : err.message);
+    res
+      .status(500)
+      .json({ error: "Failed to send SMS. Please try again later." });
+  }
+});
+
+router.get("/messages", (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "../Views/messageslist.html"));
+});
+
+// API endpoint to fetch messages
+router.get("/api/messages", async (req: Request, res: AppRes) => {
+  try {
+    const useri = res.locals.user;
+    if (!useri) {
+      return res.status(404).send("User not found.");
+    }
+    const userId = useri._id;
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    // Find the user by their ID and populate the messages field
+    const user = await SignModel.findById(userId).populate("messages").exec();
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Send the user's messages as a response
+    res.status(200).json({ messages: user.messages });
+  } catch (error) {
+    console.error("Error fetching messages:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+export default router;
